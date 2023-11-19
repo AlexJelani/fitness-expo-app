216b498189dfa258a7dda9745460919c9227b34d
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2023-11-19 23:46:03 +0900

    first commit added expo router

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function Index() {
+    return (
+        <View>
+            <Text>our first screen 1</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+
