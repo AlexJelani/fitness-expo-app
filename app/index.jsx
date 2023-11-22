e9c8b8797f3b92c372a1cd3d84524b5d9be5ff8f
alexjelani <jelanialexander82@gmail.com>
2023-11-22 19:40:10 +0900

    added welcome text

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import {StatusBar, View, Image} from 'react-native';
+import {StatusBar, View, Image, Text} from 'react-native';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { LinearGradient } from 'expo-linear-gradient';
 
@@ -7,6 +7,15 @@ export default function Index() {
         <View className="flex-1 flex justify-end">
             <StatusBar style="light"/>
             <Image className="h-full w-full absolute" source={require("../assets/images/welcome.png")}/>
+            <LinearGradient colors={['transparent', '#18181b']}
+                            style={{width:wp(100), height:hp(70)}}
+                            start={{x:0.5, y:0}}
+                            end={{x:0.5, y:0.8}}
+                            className="flex justify-end pb-12 space-y-8"
+            />
+            <View className="flex items-center">
+                <Text className="text-white">Best Workouts</Text>
+            </View>
         </View>
     );
 }
