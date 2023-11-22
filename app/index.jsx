71ee1b0bf7e647b7c14d8d9117db46ca0dfcee14
alexjelani <jelanialexander82@gmail.com>
2023-11-22 19:25:57 +0900

    added layout, images

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import {StatusBar, View, Image} from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Index() {
     return (
-        <View>
-            <Text>our first screen 1</Text>
-            <StatusBar style="auto" />
+        <View className="flex-1 flex justify-end">
+            <StatusBar style="light"/>
+            <Image className="h-full w-full absolute" source={require("../assets/images/welcome.png")}/>
         </View>
     );
 }
